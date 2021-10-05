8df70033b842b96e576056934eece8c5023ac7c3
acer <davronbekprogrammer@gmail.com>
2021-10-05 12:41:08 +0500

    salom o'zgarish

--- app/src/main/java/com/example/adiblar/HomeFragment.kt
+++ app/src/main/java/com/example/adiblar/HomeFragment.kt
@@ -20,7 +20,7 @@ private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 class HomeFragment : Fragment() {
-
+// salom o'zgarish bo'ldi
     private var param1: String? = null
     private var param2: String? = null
 
